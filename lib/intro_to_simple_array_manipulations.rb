5bf332975e7b09a74c4ec7b2898375fe771c631e
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-23 23:59:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,3 +57,17 @@ artists=["Rihanna", "Breezy","Beyonce","JB", "Tyrese","Tank"]
   artists.using_flatten=(all_artist)
   
   # => ["Cardi B","Beyonce","Rihanna", "Breezy","Snoop Dogg","JB", "Tyrese","Tank"]
+  
+  artists=["Cardi B","Beyonce","Rihanna", "Breezy","Snoop Dogg","JB", "Tyrese","Tank"]
+  delete_letter_b_artist=("Breezy","Beyonce")
+  artists.using_delete=(delete_letter_b_artist)
+  
+  # => ["Cardi B","Rihanna","Snoop Dogg","JB", "Tyrese","Tank"]
+  
+  artists=["Cardi B","Beyonce","Rihanna", "Breezy","Snoop Dogg","JB", "Tyrese","Tank"]
+  delete_artist=(4)
+  artists.using_delete_at=(delete_artist)
+  
+  #=> "Tyrese"
+  
+  
