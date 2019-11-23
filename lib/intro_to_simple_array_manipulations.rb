8931abce859a2e936080b63ffa9d2275964bd6ca
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-23 23:10:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,20 @@
-artists=["Rihanna","Tank","Breezy"]
+artists=["Rihanna","Tank"]
   next_artist="Cardi B"
   artists.push(next_artist)
   
-  # => ["Rihanna","Tank","Breezy", "Cardi B"]
+  # => ["Rihanna","Tank", "Cardi B"]
+  
+  artists=["Rihanna","Tank","Cardi B"]
+  first_artist="Cardi B"
+  artists.unshift=(first_artist)
+  
+  # => ["Cardi B","Rihanna", "Tank"]
+  
+  artists=["Cardi B","Rihanna", "Tank"]
+  last_artist="Tank"
+  artists.pop=(last_artist)
+  
+  # => ["Cardi B","Rihanna"]
+  
+  
+  
